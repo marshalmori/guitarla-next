cc7566bda6e0c9446d63742b8d9e310c1205565e
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-02-02 18:34:54 -0300

    Consultar posts na API

--- pages/blog.js
+++ pages/blog.js
@@ -1,9 +1,27 @@
 import Layout from "@/components/layout";
+import Post from "@/components/post";
 
-export default function Blog() {
+// O destructuring {posts } vem de getStaticProps
+export default function Blog({ posts }) {
   return (
     <Layout title={"Blog"} description={"Blog guitarLA, tienda de música"}>
-      <h1>Blog</h1>
+      <main>
+        <h1 className="heading">Blog</h1>
+        <div>
+          {posts?.map((post) => (
+            <Post key={post.id} post={post.attributes} />
+          ))}
+        </div>
+      </main>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const respuesta = await fetch(`${process.env.API_URL}/posts?populate=imagen`);
+  const { data: posts } = await respuesta.json();
+
+  return {
+    props: { posts },
+  };
 }
--- pages/tienda.js
+++ pages/tienda.js
@@ -29,13 +29,13 @@ export default function Tienda({ guitarras }) {
 //   };
 // }
 
-export async function getServerSideProps() {
-  const respuesta = await fetch(
-    `${process.env.API_URL}/guitarras?populate=imagen`
-  );
-  const { data: guitarras } = await respuesta.json();
+// export async function getServerSideProps() {
+//   const respuesta = await fetch(
+//     `${process.env.API_URL}/guitarras?populate=imagen`
+//   );
+//   const { data: guitarras } = await respuesta.json();
 
-  return {
-    props: { guitarras },
-  };
-}
+//   return {
+//     props: { guitarras },
+//   };
+// }
