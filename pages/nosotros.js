dc72928af3969bd1bd381c654942c0371cae418d
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-02-02 10:39:08 -0300

    Criar a página Nosotros e criar imagens avif e webp

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,4 +1,6 @@
+import Image from "next/image";
 import Layout from "@/components/layout";
+import styles from "@/styles/nosotros.module.css";
 
 export default function Nosotros() {
   return (
@@ -6,7 +8,36 @@ export default function Nosotros() {
       title={"Nosotros"}
       description={"Sobre nosotros, guitarLA, tienda de música"}
     >
-      <h1>Nosotros</h1>
+      <main className="contenedor">
+        <h2 className="heading">Nosotros</h2>
+
+        <div className={styles.contenido}>
+          <Image
+            src="/img/nosotros.jpg"
+            alt="Imagen sobre nosotros"
+            width={1000}
+            height={800}
+          />
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Praesentium quibusdam aspernatur, nobis, suscipit odio totam
+              perspiciatis facere voluptatibus sit numquam et in, unde aliquam?
+              Error modi officia itaque facilis illum? Lorem ipsum dolor sit
+              amet consectetur adipisicing elit. Magni repellat eligendi
+              cupiditate corporis, voluptates.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Praesentium quibusdam aspernatur, nobis, suscipit odio totam
+              perspiciatis facere voluptatibus sit numquam et in, unde aliquam?
+              Error modi officia itaque facilis illum? Lorem ipsum dolor sit
+              amet consectetur adipisicing elit. Magni repellat eligendi
+              cupiditate corporis, voluptates.
+            </p>
+          </div>
+        </div>
+      </main>
     </Layout>
   );
 }
