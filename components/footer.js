7347b079466505d2ae2f5db65413592785c6110f
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-02-01 23:51:30 -0300

    Criar o Footer

--- components/footer.js
+++ components/footer.js
@@ -1,3 +1,20 @@
+import Link from "next/link";
+
+import { useRouter } from "next/router";
+
 export default function Footer() {
-  return <div>footer</div>;
+  const router = useRouter();
+  return (
+    <footer>
+      <div className="contenedor">
+        <nav className="navegacion">
+          <Link href="/">Inicio</Link>
+          <Link href="/nosotros">Nosotros</Link>
+          <Link href="/blog">Blog</Link>
+          <Link href="/tienda">Tienda</Link>
+        </nav>
+        <p>Todos os direitos reservados {new Date().getFullYear()}</p>
+      </div>
+    </footer>
+  );
 }
