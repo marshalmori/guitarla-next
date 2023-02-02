79382df4cc41302d0bca2d300c9ef490eccb7a9b
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-02-02 10:18:30 -0300

    Aplicar CSS ao Footer

--- components/footer.js
+++ components/footer.js
@@ -1,19 +1,22 @@
 import Link from "next/link";
+import styles from "@/styles/footer.module.css";
 
 import { useRouter } from "next/router";
 
 export default function Footer() {
   const router = useRouter();
   return (
-    <footer>
-      <div className="contenedor">
-        <nav className="navegacion">
+    <footer className={styles.footer}>
+      <div className={`contenedor ${styles.contenido}`}>
+        <nav className={styles.navegacion}>
           <Link href="/">Inicio</Link>
           <Link href="/nosotros">Nosotros</Link>
           <Link href="/blog">Blog</Link>
           <Link href="/tienda">Tienda</Link>
         </nav>
-        <p>Todos os direitos reservados {new Date().getFullYear()}</p>
+        <p className={styles.copyright}>
+          Todos os direitos reservados {new Date().getFullYear()}
+        </p>
       </div>
     </footer>
   );
