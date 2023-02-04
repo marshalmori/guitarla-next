2f4636d584d8c22d9d43927b52b922f6a66fad34
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-02-03 22:34:45 -0300

    Mostrar o conteúdo do carrinho

--- pages/carrito.js
+++ pages/carrito.js
@@ -1,7 +1,8 @@
 import Layout from "@/components/layout";
 import styles from "../styles/carrito.module.css";
+import Image from "next/image";
 
-export default function Carrito() {
+export default function Carrito({ carrito }) {
   return (
     <Layout title="Carrito de compras">
       <main className="contenedor">
@@ -10,6 +11,31 @@ export default function Carrito() {
         <div className={styles.contenido}>
           <div className={styles.carrito}>
             <h2>Artículos</h2>
+
+            {carrito.length === 0
+              ? "Carrito Vacio"
+              : carrito.map((producto) => (
+                  <div key={producto.id} className={styles.producto}>
+                    <div>
+                      <Image
+                        width={250}
+                        height={480}
+                        src={producto.imagen}
+                        alt={producto.nombre}
+                      />
+                    </div>
+                    <div>
+                      <p className={styles.nombre}>{producto.nombre}</p>
+                      <p className={styles.precio}>
+                        $<span>{producto.precio}</span>
+                      </p>
+                      <p className={styles.subtotal}>
+                        Subtotal: $
+                        <span>{producto.cantidad * producto.precio}</span>
+                      </p>
+                    </div>
+                  </div>
+                ))}
           </div>
           <aside className={styles.resumen}>
             <h3>Resumen del Pedido</h3>
